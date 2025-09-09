306cbab29a66275ae2f71678927f19dbe970dda7
DeepVoltaire <philip@popien.net>
2025-09-09 18:58:48 +0200

    delete unused files

--- scripts/smoke_ndvi_timelapse.py
+++
@@ -1,8 +0,0 @@
-from blocks.components.util.scaffold import ee_authenticate
-from blocks.components.gee.aoi_from_spec import aoi_from_spec
-from blocks.components.gee.ndvi_acquire_process import build_ndvi_doy_composite
-
-ee_authenticate()
-aoi = aoi_from_spec({"type":"place","name":"Europe","radius_km":500})  # großzügig
-col = build_ndvi_doy_composite(ref_year=2019)
-print("OK: NDVI DOY collection size (server-side):", col.size().getInfo())
--- scripts/smoke_urban_built.py
+++
@@ -1,8 +0,0 @@
-from blocks.components.util.scaffold import ee_authenticate
-from blocks.components.gee.aoi_from_spec import aoi_from_spec
-from blocks.components.gee.urban_acquire_process import get_built_surface_image
-
-ee_authenticate()
-aoi = aoi_from_spec({"type":"place","name":"Shenzhen, China","radius_km":25})
-img = get_built_surface_image(2025).clip(aoi)
-print("OK: Urban built surface image prepared.")
